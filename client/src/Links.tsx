5e32e97260d1d99ad163fcc2a2af63f50810b51b
SCREECH115 <arekw99@gmail.com>
2023-07-08 19:40:46 +0200

    Added new button which get all links, added new edpoint to backend, button returning object

---
+++ client/src/Links.tsx
@@ -0,0 +1,17 @@
+
+
+const Links = () => {
+   return(
+      <>
+         <div className="flex justify-center mx-auto">
+            <div className="mt-5 flex-col">
+               <p>id: hdqoh8128h</p>
+               <p>original link: https://www.google.pl</p>
+               <p>date: 2023/06/01</p>
+            </div>
+         </div>
+      </>
+   )
+}
+
+export default Links
--- server/index.ts
+++ server/index.ts
@@ -14,6 +14,16 @@ app.get('/:id', async (req:Request, res:Response) => {
   }
 })
 
+app.get('/', async (req:Request, res:Response) => {
+  try {
+    const response = await axios.get(`http://127.0.0.1":8090/api/collections/links/records`)
+    console.log(response);
+    res.send(200)
+  } catch(err){
+    res.redirect('http://localhost:5173')
+  }
+})
+
 app.listen(port, () => {
    console.log(`Server is on on port ${port}`);
 })
